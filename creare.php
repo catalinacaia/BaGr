203aaaeb3d24132cf12d117a41e961208495938d
catalinacaia <caia_catalina@yahoo.com>
2018-06-11 17:40:21 +0300

    added function for landscape and portrait pass, added code for history, added history list

--- creare.php
+++ creare.php
@@ -1,44 +1,347 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>BaGr (Badge Generator)</title>
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+</head>
+	
+<body>
+<div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <h1><a href="index.html">BaGr (Badge Generator)<span class="logo_colour"></span></a></h1>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="despre.html">Despre</a></li>
+          <li class="selected"><a href="creare.php">Creare</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+<div id="site_content">
+<!--Formular de completare date-->
+<div id="wrapper">
+<div class="row">
+<div class="column">
+
 <?php
-$row = 1;
-if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
-    }
-    fclose($handle);
-}
+session_start();
 
-// cache DOM:
-var container = document.getElementById('container');
+$search = parseRequest();
+storeSearch($search);
 
-function createSVGtemplate(nume, prenume, sex, rol) {
-  var svgTemplate = '<svg width="500" height="300">' +
-    '<rect x="50" y="20" rx="20" ry="20" width="250" height="250" style="fill:red;stroke:black;stroke-width:5;opacity:0.5" />' +
-    '<text x="140" y="70" style="font-family: Times New Roman;font-size: 30px;stroke: #00ff00;fill: #0000ff;">' + 'Person:</text>' +
-    '<text x="70" y="100" style="fill:black;">Nume: ' + nume + '</text>' +
-    '<text x="70" y="130">Prenume: ' + prenume + ' </text>' +
-    '<text x="70" y="160">Sex: ' + sex + ' </text>' +
-	'<text x="70" y="160">Rol: ' + rol + ' </text>' +
-    '</svg>';
-  return document.createRange().createContextualFragment(svgTemplate)
+include "form.php";
+
+$searches = $_SESSION['searches'];
+
+
+function storeSearch($search) {
+	if (!isset($_SESSION['searches'])) {
+		$_SESSION['searches'] = [];
+	}
+
+	if (!$search->isEmpty()) {
+		$_SESSION['searches'][] = $search;
+	}
 }
 
 
-
-function createTemplates(data) {
-  let documentFragment = document.createDocumentFragment();
-  for (let i = 0; i < data.length; i++) {
-    let filledTemplate = createSVGtemplate(data[i].nume, data[i].prenume, data[i].sex, data[i].rol);
-    documentFragment.appendChild(filledTemplate);
-  };
-  container.appendChild(documentFragment);
+function parseRequest() {
+	$search = new SearchRequest;
+	$search->nume = !empty($_GET['nume']) ? $_GET['nume'] : "";
+	$search->prenume = !empty($_GET['prenume']) ? $_GET['prenume'] : "";
+	$search->sex = !empty($_GET['sex']) ? $_GET['sex'] : "";
+	$search->rol = !empty($_GET['rol']) ? $_GET['rol'] : "";
+    $search->cul = !empty($_GET['cul']) ? $_GET['cul'] : "";
+	$search->font = !empty($_GET['font']) ? $_GET['font'] : "";
+	$search->format = !empty($_GET['format']) ? $_GET['format'] : "";
+	return $search;
 }
 
-(function () {
-  createTemplates(data)
-}())
+/**
+ * search request
+ */
+class SearchRequest
+{
+	public $nume = "";
+	public $prenume = "";
+	public $sex = "";
+	public $rol = "";
+	public $cul = "";
+	public $font = "";
+	public $format = "";
+
+	function toQueryString() {
+		$params = [
+				'nume' => $this->nume,
+				'prenume' => $this->prenume,
+				'sex' => $this->sex,
+				'rol'=> $this->rol,
+				'cul'=> $this->cul,
+				'font'=> $this->font,
+				'format'=> $this->format
+		];
+
+		return http_build_query($params);
+	}
+
+	function isEmpty() {
+		return !$this->nume || !$this->prenume || !$this->sex || !$this->rol || !$this->cul || !$this->font || !$this->format;
+	}
+
+	function numeAsObject() {
+		return new DateTime($this->nume);
+	}
+
+	function prenumeAsObject() {
+		return new DateTime($this->prenume);
+	}
+	
+	function sexAsObject() {
+		return new DateTime($this->sex);
+	}
+	
+	function rolAsObject() {
+		return new DateTime($this->rol);
+	}
+	
+	function culAsObject() {
+		return new DateTime($this->cul);
+	}
+	
+	function fontAsObject() {
+		return new DateTime($this->font);
+	}
+	
+	function formatAsObject() {
+		return new DateTime($this->format);
+	}
+
+}
 
 ?>
+</div>
+<div class="column">
+<ul>
+	<?php
+	foreach ($searches as $s) {
+	?>
+	 <li><a href="creare.php?<?php echo $s->toQueryString() ?>">
+	 		<?php echo $s->nume?> - <?php echo $s->prenume?> - <?php echo $s->sex?> - <?php echo $s->rol?> - <?php echo $s->cul?> - <?php echo $s->font?> - <?php echo $s->format?>
+		  </a></li>
+	<?php
+	}
+	?>
+</ul>
+</div>
+
+</div>
+<br><br>
+
+<!--Formular incarcare logo-->
+<form action="uploadimg.php" method="post" enctype="multipart/form-data">
+    Selectati logo (format acceptat .jpg):
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Logo" name="submit">
+</form>
+<br><br>
+	
+
+<!--Afisarea ecusonului-->
+
+<canvas id="canvas1" width="250" height="380" style="border:1px solid #000000;">
+</canvas>
+<br><br>
+<button onclick="stergedate()">Creaza alt ecuson</button>
+<button onclick="SaveEcuson1()">Descarca Ecuson</button>
+<button type="button" onclick="incarcalogo1()">Incarca Logo</button>
+<br><br>
+
+<canvas id="canvas2" width="380" height="230" style="border:1px solid #000000;">
+</canvas>
+<br><br>
+<button onclick="stergedate()">Creaza alt ecuson</button>
+<button onclick="SaveEcuson2()">Descarca Ecuson</button>
+<button type="button" onclick="incarcalogo2()">Incarca Logo</button>
+
+
+</div>
+
+<!--Functia de incarcare a logo-ului in canvas portrait-->
+<script>
+var canvas = document.getElementById('canvas1'),
+context = canvas.getContext('2d');
+
+make_base();
+
+function incarcalogo1()
+{
+  logo_image = new Image();
+  logo_image.src = 'uploads/logo.jpg';
+  logo_image.onload = function(){
+  context1.drawImage(logo_image, 10, 10, 40, 40);
+  }
+}
+</script>
+
+<!--Functia de incarcare a logo-ului in canvas landscape-->
+<script>
+var canvas = document.getElementById('canvas2'),
+context = canvas.getContext('2d');
+
+make_base();
+
+function incarcalogo2()
+{
+  logo_image = new Image();
+  logo_image.src = 'uploads/logo.jpg';
+  logo_image.onload = function(){
+  context.drawImage(logo_image, 10, 10, 40, 40);
+  }
+}
+</script>
+
+<!--Functia de salvare a ecusonului portrait-->
+<script>
+function SaveEcuson1() {
+		var link = document.createElement('a');
+                link.download = "ecuson.jpg";
+                link.href = canvas1.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                link.click();
+		      }
+</script>
+
+<!--Functia de salvare a ecusonului landscape-->
+<script>
+function SaveEcuson2() {
+		var link = document.createElement('a');
+                link.download = "ecuson.jpg";
+                link.href = canvas2.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                link.click();
+		      }
+</script>
+
+<!--Sterge datele din ecuson pentru a se putea crea altul-->
+<script>
+function stergedate() {
+    location.reload();
+}
+</script>
+
+<!--Creaza ecusonul portrait-->
+<script>
+var c = document.getElementById("canvas1");
+var context = c.getContext("2d");
+context.fillStyle = '#fff';
+context.fillRect(0, 0, canvas.width, canvas.height);
+// Create gradient
+var grd = context.createLinearGradient(10,10,200,0);
+grd.addColorStop(0,"#00BFFF");
+context.lineWidth = 2;
+// Fill with gradient
+context.fillStyle = grd;
+context.fillRect(0,60,250,70);
+context.font = "30px Comic Sans MS";
+context.fillStyle = 'black';
+context.strokeText("PASS: ", 80, 110);
+</script>
+
+<!--Creaza ecusonul landscape-->
+<script>
+var c = document.getElementById("canvas2");
+var context = c.getContext("2d");
+context.fillStyle = '#fff';
+context.fillRect(0, 0, canvas.width, canvas.height);
+// Create gradient
+var grd = context.createLinearGradient(10,10,200,0);
+grd.addColorStop(0,"#00BFFF");
+context.lineWidth = 2;
+// Fill with gradient
+context.fillStyle = grd;
+context.fillRect(0,30,380,70);
+context.font = "30px Comic Sans MS";
+context.fillStyle = 'black';
+context.strokeText("PASS: ", 140, 80);
+</script>
+
+<!--Completeaza datele in ecuson-->
+<script>
+function create() {
+
+    var canvas1 = document.getElementById("canvas1");
+    var context1 = canvas1.getContext("2d");
+	var canvas2 = document.getElementById("canvas2");
+    var context2 = canvas2.getContext("2d");
+   
+   if(document.getElementById("format").value == "portrait") {
+        context1.font ="15px" + ' ' + document.getElementById("font").value;
+        context1.fillStyle = document.getElementById("cul").value;
+        context1.fillText("Nume: ", 30, 230);
+        context1.fillText(document.getElementById("nume").value, 150, 230);
+        context1.fillText("Prenume: ", 30, 250);
+        context1.fillText(document.getElementById("prenume").value, 150, 250);
+        context1.fillText("Sex: ", 30, 270);
+        context1.fillText(document.getElementById("sex").value, 150, 270);
+        context1.fillText("Rol: ", 30, 290);
+        context1.fillText(document.getElementById("rol").value, 150, 290);}
+	  else {
+	     context2.font ="15px" + ' ' +  document.getElementById("font").value;
+         context2.fillStyle = document.getElementById("cul").value;
+         context2.fillText("Nume: ", 40, 130);
+         context2.fillText(document.getElementById("nume").value, 160, 130);
+         context2.fillText("Prenume: ", 40, 150);
+         context2.fillText(document.getElementById("prenume").value, 160, 150);
+         context2.fillText("Sex: ", 40, 170);
+         context2.fillText(document.getElementById("sex").value, 160, 170);
+         context2.fillText("Rol: ", 40, 190);
+         context2.fillText(document.getElementById("rol").value, 160, 190);}
+}
+</script>
+<br><br><br><br>
+<form id="passes2">  
+            Creaza mai multe ecusoane folosind un document .csv:<br><br>
+	               Culoarea text: <div class="select-wrapper">
+			                  <select name="cul" id="cul">
+				                 <option value="">- Culoarea textului -</option>
+				                 <option value="red">Rosu</option>
+			               	     <option value="blue">Albastru</option>
+				                 <option value="black">Negru</option>
+			                  </select>
+			                 </div>
+                   Font text: <div class="select-wrapper">
+			                  <select name="font" id="font">
+				                 <option value="">- Font text -</option>
+				                 <option value="15px Arial">Arial</option>
+			               	     <option value="15px Times New Roman">Times New Roman</option>
+				                 <option value="15px Georgia">Georgia</option>
+                                 <option value="15px Comic Sans MS">Comic Sans MS</option>
+			               	     <option value="15px Lucida Sans Unicode">Lucida Sans Unicode</option>
+				                 <option value="15px Courier New">Courier New</option>
+			                  </select>
+			                 </div>
+				   Format ecuson: <div class="select-wrapper">
+				                    <select name="format" id="format">
+							           <option value="">- Format ecuson -</option>
+							           <option value="portrait">Portrait</option>
+				                       <option value="landscape">Landscape</option>
+			                        </select>
+								  </div>
+</form>
+<!-- Incarcare fisier cu date pentru ecusoane -->
+<form action="uploadtxt.php" method="post" enctype="multipart/form-data">
+    Selectati fisierul cu datele pentru ecusoane (format acceptat .txt):
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Fisier" name="submit">
+</form><br><br>
+<a href="crearecsv.php" class="button">Creaza ecuson</a>
+
+</div>
+</div>
+
+</body>
+</html>
