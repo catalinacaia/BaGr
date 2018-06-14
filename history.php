1cd7107a14c52c851a48f3545da2ba7c4e97e059
catalinacaia <caia_catalina@yahoo.com>
2018-06-14 11:03:03 +0300

    added the code to introduce pass color to history list

--- history.php
+++ history.php
@@ -3,7 +3,7 @@
 	foreach ($searches as $s) {
 	?>
 	 <li><a href="search.php?<?php echo $s->toQueryString() ?>">
-	 		<?php echo $s->nume?> - <?php echo $s->prenume?> - <?php echo $s->sex?> - <?php echo $s->rol?> - <?php echo $s->cul?> - <?php echo $s->font?> - <?php echo $s->format?>
+	 		<?php echo $s->nume?> - <?php echo $s->prenume?> - <?php echo $s->sex?> - <?php echo $s->rol?> - <?php echo $s->cul?> - <?php echo $s->cul1?> - <?php echo $s->font?> - <?php echo $s->format?>
 		  </a></li>
 	<?php
 	}
